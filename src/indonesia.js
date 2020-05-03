e35f2709f8e80463364567cae2aee09f59d2dc9c
Satya Bagus Dwiatmaja <dwii5359@azalelnation.com>
2020-05-03 19:21:45 +0700

    Update indonesia.js

--- src/indonesia.js
+++ src/indonesia.js
@@ -6,45 +6,49 @@ var config = require("./config.json");
 console.log("Webhook Connected.");
 
 var job = new CronJob(
-  "5 6,18 * * *",
+  "5 18 * * *",
   async function() {
     const { WebhookClient, MessageEmbed } = require("discord.js");
     const hook = new WebhookClient(config.webhookID, config.webhookToken);
     
-    let { body: corona } = await request.get(
-      "https://api.kawalcorona.com/"
-    );
     let { body: indo } = await request.get(
-      "https://api.kawalcorona.com/indonesia"
+      "https://indonesia-covid-19-api.now.sh/api"
     );
     let { body: indoprov } = await request.get(
-      "https://api.kawalcorona.com/indonesia/provinsi"
+      "https://indonesia-covid-19-api.now.sh/api/provinsi"
     );
 
-    let arrey = indoprov
-      .slice(0, 10)
-      .map(
-        (item, i) => `${i + 1}. ${item.attributes.Provinsi}: ${item.attributes.Kasus_Posi} Positif | ${item.attributes.Kasus_Semb} Sembuh | ${item.attributes.Kasus_Meni} Meninggal`)
-      .join("\n");
+  let arrey = indoprov.data
+  .sort((a, b) => a.kasusPosi < b.kasusPosi)
+  .slice(0, 10)
+  .map(
+    (item, i) =>
+      `${i + 1}. ${item.provinsi}: ${item.kasusPosi} Positif | ${
+        item.kasusSemb
+      } Sembuh | ${item.kasusMeni} Meninggal`
+  )
+  .join("\n");
+
 
     const embed = new MessageEmbed()
       .setAuthor(
-        "Indonesia COVID-19 Daily Webhook",
+        "COVID-19 in Indonesia Daily Webhook",
         "https://cdn.glitch.com/c8c4439d-fc47-4f12-a21c-755bac53a055%2Frsz_200212152442-tak-i.png?v=1585125357606"
       )
       .setColor("RED")
-      .setDescription("Statistik Provinsi 10 Teratas \n```" + arrey + "```")
+      .setDescription("Statistik 10 Provinsi paling rawan \n```" + arrey + "```")
       .addField(
-        "Total",
-        `**Positif:** ${indo[0].positif} | **Sembuh:** ${indo[0].sembuh} | **Meninggal:** ${indo[0].meninggal}`
+        "Total " + indo.jumlahKasus + " kasus",
+        `**Positif:** ${indo.perawatan} | **Sembuh:** ${indo.sembuh} | **Meninggal:** ${indo.meninggal}`
+      )
+      .addField(
+        "More Info:",
+        "[Kunjungi Website Resmi COVID-19 Indonesia](https://covid19.go.id/)"
       )
       .setFooter(
-        "Last Updated: " +
-          moment(
-            corona.find(x => x.attributes.Country_Region === "Indonesia")
-              .attributes.Last_Update
-          ).format("LLLL")
-      );
+        "Menggunakan data dari https://github.com/mathdroid/indonesia-covid-19-api"
+      )
+      .setTimestamp();
 
     hook.send(embed);
     console.log("Feed Delivery Success!");
