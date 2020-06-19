f9c6ec54f9216b312b6a7c070dca045294976f51
ItzMeDwii <dwii5359@azalelnation.com>
2020-06-19 14:11:02 +0700

    Add toggler in config.json.

--- app.ts
+++ app.ts
@@ -1,3 +1,8 @@
+const config = require("./src/config.json");
 require("dotenv").config();
 
+if (config.settings.enableID) {
 require("./src/indonesia");
+} else {
+console.log("Indonesia Webhook disabled.");
+}
