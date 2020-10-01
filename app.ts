66a1a56e0d8c11134505f9e5948c06ee1123765a
Satya Bagus Dwiatmaja <harnowo991943@gmail.com>
2020-10-01 11:22:35 +0700

    update: add global

--- app.ts
+++ app.ts
@@ -6,3 +6,9 @@ require("./src/indonesia");
 } else {
 console.log("Indonesia Webhook disabled.");
 }
+
+if (config.settings.enableGlobal) {
+require("./src/global");
+} else {
+console.log("Global Webhook disabled.");
+}
